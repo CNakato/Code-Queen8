951e38b0afacc1210fd240f18b0de89062de91d3
CNakato <clairenakato28@gmail.com>
2023-03-28 12:06:54 +0300

    JS

---
+++ index.js
@@ -0,0 +1,11 @@
+//declare a variable called x
+//this is the next line
+
+
+/**declare a variable x and assign */
+
+
+var x= "Claire";
+console.log(x);
+
+document.getElementById("message").innerHTML = "Hooray you have used Javascript";
